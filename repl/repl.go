da2e19ec43c6548de828aff189bf11837aa50fe5
Ruby Bui <ruby@coderpush.com>
2025-05-27 15:45:02 -0700

    add console (read eval print loop)

---
+++ repl/repl.go
@@ -0,0 +1,41 @@
+//read eval print loop
+// sometimes called "console" & "interactive mode"
+// read: read input from user
+// eval: evaluate input
+// print: print result
+// loop: repeat
+
+// REPL is a simple interactive shell that allows you to interact with a program
+// by typing in commands and seeing the results.
+
+package repl 
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
