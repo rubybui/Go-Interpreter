ba25462249c10241f64009c1f0553f7519e3c690
Ruby Bui <ruby@coderpush.com>
2025-05-28 13:46:11 -0700

    add ability to print to debug ast

--- ast/ast.go
+++ ast/ast.go
@@ -9,10 +9,14 @@ package ast
 
 // Every node in AST have to implement the Node interface -> provide a Tokenliteral() method
 // that return the literal value of the token it's associate with 
-import "monkey/token"
+import (
+	"monkey/token"
+	"bytes"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string // for easier printing of AST node 
 }
 
 type Statement interface {
@@ -29,6 +33,22 @@ type Program struct {
 	Statements []Statement
 }
 
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+	out.WriteString(s.String())
+	}
+	return out.String()
+}
+
 
 type LetStatement struct {
 	Token token.Token
@@ -36,17 +56,54 @@ type LetStatement struct {
 	Value Expression
 }
 
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal}
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+
 type ReturnStatement struct {
 	Token token.Token
 	ReturnValue Expression
 }
 
-func (ls *LetStatement) statementNode() {}
-
-func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal}
-
 func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal}
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+
+type ExpressionStatement struct {
+	Token token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal}
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
 
 type Identifier struct {
 	Token token.Token
@@ -55,14 +112,9 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal}
-
-
-
-func (p *Program) TokenLiteral() string {
-	if len(p.Statements) > 0 {
-		return p.Statements[0].TokenLiteral()
-	} else {
-		return ""
-	}
+func (i *Identifier) String() string {
+	return i.Value
 }
+
+
 
