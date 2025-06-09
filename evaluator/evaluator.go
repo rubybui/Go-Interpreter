745a585ed46bb9cf75d318a1e2c549075948191c
Ruby Bui <buihongngoc.hnams@gmail.com>
2025-06-08 22:02:16 -0700

    add eval bang

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -4,6 +4,7 @@ import (
 "monkey/object"
 )
 var (
+	NULL = &object.Null{}
 	TRUE = &object.Boolean{Value: true}
 	FALSE = &object.Boolean{Value: false}
 )
@@ -19,6 +20,9 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
@@ -32,11 +36,32 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	return result
 }
 
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+		case "!":
+			return evalBangOperatorExpression(right)
+		default:
+			return NULL
+	}
+}
 
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+		case TRUE:
+			return FALSE
+		case FALSE:
+			return TRUE 
+		case NULL:
+			return TRUE
+		default: 
+			return FALSE
+	}
 
+}
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
 	if input {
 		return TRUE
 	}
 	return FALSE
 }
+
