c7d191c0dc4425d97b37f64ffa1c7d34dfb8d4f9
Ruby Bui <buihongngoc.hnams@gmail.com>
2025-06-17 16:48:27 -0700

    add minus prefix evaluator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -40,6 +40,8 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 		case "!":
 			return evalBangOperatorExpression(right)
+		case "-":
+			return evalMinusPrefixOperatorExpression(right)
 		default:
 			return NULL
 	}
@@ -58,6 +60,15 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 	}
 
 }
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
+}
+
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
 	if input {
 		return TRUE
