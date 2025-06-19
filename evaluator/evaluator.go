0f321586bcbc05d968e61f9aabe30d7811dce906
Ruby Bui <buihongngoc.hnams@gmail.com>
2025-06-18 23:53:19 -0700

    add program evaluator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -11,8 +11,7 @@ var (
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// Statements
-	case *ast.Program:
-		return evalStatements(node.Statements)
+
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 	// Expressions
@@ -28,11 +27,13 @@ func Eval(node ast.Node) object.Object {
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
 	case *ast.BlockStatement:
-		return evalStatements(node.Statements)
+		return evalBlockStatement(node)
 	case *ast.IfExpression:
 		return evalIfExpression(node)
 	case *ast.ReturnStatement:
 		return evalReturnStatements(node)
+	case *ast.Program:
+		return evalProgram(node)
 
 	}
 
@@ -143,6 +144,27 @@ func evalReturnStatements(node *ast.ReturnStatement) object.Object {
 	return &object.ReturnValue{Value: value}
 }
 
+func evalProgram(program *ast.Program) object.Object {
+	var result object.Object
+	for _, statement := range program.Statements {
+		result = Eval(statement)
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
+	}
+	return result
+}
+
+func evalBlockStatement(block *ast.BlockStatement) object.Object {
+	var result object.Object
+	for _, statement := range block.Statements {
+		result = Eval(statement)
+		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
+			return result
+		}
+	}
+	return result
+}
 
 func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
 	if right.Type() != object.INTEGER_OBJ {
