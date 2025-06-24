bedce8428bda37514a30abb7eb022f8d2e8e7607
Ruby Bui <buihongngoc.hnams@gmail.com>
2025-06-24 00:12:17 -0700

    define an internal representation of functions in our object system and add support for function calls to Eval. to support higher order function

--- object/object.go
+++ object/object.go
@@ -1,6 +1,9 @@
 package object
 import (
+	"bytes"
 	"fmt"
+	"monkey/ast"
+	"strings"
 )
 type ObjectType string
 const (
@@ -10,6 +13,8 @@ const (
 	STRING_OBJ = "STRING"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ = "ERROR"
+	FUNCTION_OBJ = "FUNCTION"
+
 )
 
 type Object interface {
@@ -59,21 +64,24 @@ func (e *Error) Type() ObjectType { return ERROR_OBJ }
 func (e *Error) Inspect() string { return "ERROR: " + e.Message }
 
 
-func NewEnvironment() *Environment {
-	s := make(map[string]Object)
-	return &Environment{store: s}
+type Function struct {
+	Parameters []*ast.Identifier
+	Body *ast.BlockStatement
+	Env *Environment
 }
 
-type Environment struct {
-	store map[string]Object
-}
-
-func (e *Environment) Get(name string) (Object, bool) {
-	obj, ok := e.store[name]
-	return obj, ok
-}
-
-func (e *Environment) Set(name string, val Object) Object {
-	e.store[name] = val
-	return val
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+	return out.String()
 }
